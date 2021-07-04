b92d0c782358116e9016ddbcb2e629f4108c158d
Zidni Rif'an <Zidni Rif'an>
2021-07-05 05:35:19 +0700

    fix bug id undefined

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -40,7 +40,7 @@ class UsersService {
 
   async verifyUserCredential(username, password) {
     const query = {
-      text: 'SELECT username, password FROM users WHERE username = $1',
+      text: 'SELECT id, password FROM users WHERE username = $1',
       values: [username],
     };
 
